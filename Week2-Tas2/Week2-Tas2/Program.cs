3d2ffee3a8d97d8b7aaec2eae70ead0b955eb681
nilayyaren <58950382+nilayyaren@users.noreply.github.com>
2020-01-05 00:08:32 +0300

    Methods
    This task include method with arithmatic operations.

---
+++ Week2-Tas2/Week2-Tas2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Week2_Tas2
+{
+    class Program
+    {
+        public static double Process(int a, int b, char c)
+        {
+            if (c == '+')
+            {
+                return a + b;
+            }
+            if(c == '-')
+            {
+                return a - b;
+            }
+            if(c=='*')
+            {
+                return a * b;
+            }
+            if(c=='/')
+            {
+                return 1.0f*a / b;
+            }
+            else
+                return 0;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter the first number");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("enter the second number");
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("choose the operator: '+' , '-' , '*' , '/'");
+            char c = Convert.ToChar(Console.ReadLine());
+            Console.WriteLine(Process(a, b, c));
+            Console.ReadLine();
+        }
+    }
+}
